8898b4a4c13a1610d871b0870ab9d6085d05f73c
Nando Vieira <fnando.vieira@gmail.com>
2012-04-22 11:34:02 -0300

    Added logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Hackerboard::Application.routes.draw do
   controller :sessions do
     get "/login", :action => :new
     post "/login", :action => :create, :as => false
+    get "/logout", :action => :destroy
   end
 end
