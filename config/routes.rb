cd58e8d0d41ac6b9a21ed8c368a1483be79a2661
Nando Vieira <fnando.vieira@gmail.com>
2012-04-28 10:18:01 -0300

    User can reply a given question.

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,17 @@ Hackerboard::Application.routes.draw do
   controller :users do
     get "/signup", :action => :new
     post "/signup", :action => :create, :as => false
+    get "/users/:id", :action => :show, :as => :user
   end
 
   controller :questions do
     get "/questions/new", :action => :new, :as => :new_question
     post "/questions/new", :action => :create, :as => false
     get "/questions/:id", :action => :show, :as => :question
+  end
+
+  controller :replies do
+    post "/questions/:question_id/replies/new", :action => :create, :as => :new_reply
   end
 
   controller :sessions do
--- spec/integration/add_reply_spec.rb
+++ spec/integration/add_reply_spec.rb
@@ -1,15 +1,11 @@
+# -*- encoding: utf-8 -*-
 require "spec_helper"
 
 describe "Add reply" do
-  context "when logged in" do
-    let!(:user) { users(:john) }
-    let!(:category) { categories(:ruby) }
-    let!(:question) do
-      FactoryGirl.create(:question, {
-        :user => user, :category => category
-      })
-    end
+  let(:user) { users(:john) }
+  let(:question) { questions(:about_rails) }
 
+  context "when logged in" do
     context "with valid data" do
       before do
         login :with => user.email
@@ -33,9 +29,30 @@ describe "Add reply" do
     end
 
     context "with invalid data" do
+      before do
+        login :with => user.email
+        visit question_path(question)
+
+        click_button "Enviar resposta"
+      end
+
+      it "displays error message" do
+        page.should have_content("Você precisa preencher sua resposta.")
+      end
+
+      it "redirects to the question page" do
+        current_path.should eql(question_path(question))
+      end
     end
   end
 
   context "when unlogged" do
+    before do
+      visit question_path(question)
+    end
+
+    it "displays message" do
+      page.should have_content("Você precisa efetuar o login para responder.")
+    end
   end
 end
